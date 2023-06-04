35004d9a6ac1698ca63d872b8f641ca33e2cbf03
julienhatemian <julien-hat.62@hotmail.fr>
2023-06-05 00:52:15 +0200

    Init Bootstrap 5.2

--- assets/partials/footer.php
+++ assets/partials/footer.php
@@ -1,2 +1,5 @@
 </body>
 </html>
+<script type="text/javascript" src="./assets/public/js/script.js"></script>
+<script type="text/javascript" src="./assets/public/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="./assets/public/js/bootstrap.min.js"></script>

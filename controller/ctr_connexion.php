dd5211a856cff37bea264d6dc5deeb4dee062b0e
julienhatemian <julien-hat.62@hotmail.fr>
2023-06-19 03:05:09 +0200

    Minor change connexion message

--- controller/ctr_connexion.php
+++ controller/ctr_connexion.php
@@ -7,7 +7,7 @@ try
     $cls_user = new cls_user();
     $data = $cls_user->getConnected( $_POST[ 'login' ], $_POST[ 'password' ] );
 
-    $_SESSION[ 'alert' ][ 'success' ] = "Bienvenue";
+    $_SESSION[ 'alert' ][ 'success' ] = "Bienvenue sur votre totool personnelle !";
 }
 catch( Exception $e )
 {
